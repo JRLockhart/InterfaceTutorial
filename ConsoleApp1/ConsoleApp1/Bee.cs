d557dc336c978c28ab8c10c695e40af301c0302f
James Lockhart <james.lockhart117@gmail.com>
2018-02-06 16:14:53 +0300

    added interface

---
+++ ConsoleApp1/ConsoleApp1/Bee.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Bee : IStingPatrol
+    {
+        int IStingPatrol.AlertLevel => throw new NotImplementedException();
+
+        int IStingPatrol.StingerLength { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+
+        bool IStingPatrol.LookForEnemies()
+        {
+            throw new NotImplementedException();
+        }
+
+        int IStingPatrol.SharpenStinger(int length)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
